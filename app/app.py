7e808160e6b8b7a3009642f358f1d00ddd13a91e
V0vaG <its_a_vio@hotmail.com>
2025-01-24 15:42:52 +0200

    q

--- app/app.py
+++ app/app.py
@@ -56,8 +56,6 @@ def end_point(name):
 
 @app.route('/send_command', methods=['POST'])
 def send_command():
-    import json
-
     data = request.get_json()
     if not data:
         return {"error": "Invalid data"}, 400
@@ -83,17 +81,24 @@ def send_command():
     try:
         radio.stopListening()
 
-        # Set channel, writing pipe, and reading pipe
+        # Set channel, writing pipe, reading pipe, data rate, and PA level
         channel = int(endpoint["radio"][0]["channel"])
         write_pipe = endpoint["radio"][0]["write_pipe"]
         read_pipe = endpoint["radio"][0]["read_pipe"]
+        data_rate = endpoint["radio"][0]["data_rate"]
+        pa_level = endpoint["radio"][0]["pa_level"]
+
+        # Apply configurations
+        data_rates = {"1MBPS": RF24_1MBPS, "2MBPS": RF24_2MBPS, "250KBPS": RF24_250KBPS}
+        pa_levels = {"MIN": RF24_PA_MIN, "LOW": RF24_PA_LOW, "HIGH": RF24_PA_HIGH, "MAX": RF24_PA_MAX}
 
         radio.setChannel(channel)
         radio.openWritingPipe(write_pipe.encode('utf-8'))
         radio.openReadingPipe(1, read_pipe.encode('utf-8'))
+        radio.setDataRate(data_rates[data_rate])
+        radio.setPALevel(pa_levels[pa_level])
 
-        # Debugging: Check current settings
-        print(f"Configured RF24 - Channel: {channel}, Write Pipe: {write_pipe}, Read Pipe: {read_pipe}")
+        print(f"Configured RF24 - Channel: {channel}, Write Pipe: {write_pipe}, Read Pipe: {read_pipe}, Data Rate: {data_rate}, PA Level: {pa_level}")
 
         # Construct the message
         message = f"ardu 0 set {toggle_pin} {state}"
@@ -321,6 +326,8 @@ def add_end_point():
         channel = request.form.get('channel')
         read_pipe = request.form.get('read_pipe')
         write_pipe = request.form.get('write_pipe')
+        data_rate = request.form.get('data_rate')
+        pa_level = request.form.get('pa_level')
 
         # Prepare the data structure
         new_end_point = {
@@ -329,7 +336,9 @@ def add_end_point():
                 {
                     "channel": channel,
                     "read_pipe": read_pipe,
-                    "write_pipe": write_pipe
+                    "write_pipe": write_pipe,
+                    "data_rate": data_rate,
+                    "pa_level": pa_level
                 }
             ],
             "toggles": [
@@ -361,6 +370,7 @@ def add_end_point():
 
     # If it's a GET request, render the add_end_point.html page
     return render_template('add_end_point.html')
+
 
 @app.route('/iot.html')
 def iot():
